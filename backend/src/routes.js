4d92d2283cb978a3a6945656e886ea80b334b67e
Gustavo Richard <gustavo.richard@outlook.com.br>
2020-03-25 17:49:03 -0300

    Desenvolvimento do backend do Be The Hero da semana omnistack 11

---
+++ backend/src/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');3
+const routes = express.Router();
+
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
+
+
+routes.post('/sessions', SessionController.create);
+routes.get('/profile', ProfileController.index);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+
+
+module.exports = routes;
