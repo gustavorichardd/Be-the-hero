39111a7a89fd4c472cba5e7ceae4098840e7ec80
Gustavo Richard <gustavo.richard@outlook.com.br>
2020-03-26 15:37:28 -0300

    Criado o frontend da Aplicação

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -15,6 +15,7 @@ routes.post('/ongs', OngController.create);
 
 routes.post('/incidents', IncidentController.create);
 routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 
 module.exports = routes;
